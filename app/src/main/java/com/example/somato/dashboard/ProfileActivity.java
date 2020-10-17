2da4d54627d65c9ee338d1fde36f3ff05239ccb6
apple <apple@apples-MacBook-Pro.local>
2020-10-17 13:58:09 +0530

    hotfixes

--- app/src/main/java/com/example/somato/dashboard/ProfileActivity.java
+++ app/src/main/java/com/example/somato/dashboard/ProfileActivity.java
@@ -3,14 +3,19 @@ package com.example.somato.dashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.somato.R;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    ImageView backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        backBtn = findViewById(R.id.back);
+        backBtn.setOnClickListener(view -> {
+            onBackPressed();
+        });
     }
 }
