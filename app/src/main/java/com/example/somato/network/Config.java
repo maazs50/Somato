e6ae9898e270dbb5131fce06b9d71ffbaa6b8162
apple <apple@apples-MacBook-Pro.local>
2020-10-17 14:18:47 +0530

    hotfixes

--- app/src/main/java/com/example/somato/network/Config.java
+++ app/src/main/java/com/example/somato/network/Config.java
@@ -3,6 +3,6 @@ package com.example.somato.network;
 public class Config {
     public static final String BASE_URL = "https://developers.zomato.com/";
     public static final String API_KEY = "c44f8e2c7df1c4b929c3e1169ecba0b5";
-    public static String lattitude = "28.65381";
-    public static String longitude = "77.22897";
+    public static String lattitude = "12.9517185";
+    public static String longitude = "77.705678";
 }
